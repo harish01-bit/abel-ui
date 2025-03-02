efd01365d7722c81b3d8144ba0ff982c64bbd9f0
Midthun <midthun.subbian@hebronsystems.co.uk>
2025-03-02 12:54:17 +0000

    Update chat-response.tsx

--- src/components/chat-window/main-chat/chat-response/chat-response.tsx
+++ src/components/chat-window/main-chat/chat-response/chat-response.tsx
@@ -1,11 +1,16 @@
 import { Fragment } from "react/jsx-runtime";
 import RenderText from "../../../ui-component/render-text/render-text";
+import { PiSpeakerHighBold } from "react-icons/pi";
 
 type Props = {
     message: string
 }
 
 function ChatResponseComponent({ message }: Props) {
+    const handleSpeak = () => {
+        const msg = new SpeechSynthesisUtterance(message);
+        window.speechSynthesis.speak(msg);
+      };
     return (
         <Fragment >
             <div className="avtar" />
@@ -14,6 +19,9 @@ function ChatResponseComponent({ message }: Props) {
                 <div className="content  text-wrapper">
                     <RenderText text={message} />
                 </div>
+                <div onClick={handleSpeak} style={{cursor:"pointer"}}>
+                <PiSpeakerHighBold className="absolute bottom-2 right-2 w-5 h-5 text-gray-500 hover:text-blue-500 cursor-pointer transition-transform transform hover:scale-110" />
+           </div>
             </div>
 
         </Fragment>
